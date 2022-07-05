6be753d99c25fb4c798e2e2e58070bd10203a2fd
Abhishek <abhiswc29@gmail.com>
2022-07-05 21:52:08 +0530

    Day2-1

---
+++ Day2/findDuplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static int findDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            if (!set.add(nums[i])) {
+                return nums[i];
+            }
+        }
+        return len;
+    }
+}
