62ccbecb40b9278a60315af3ba1f93a624753b25
Abhishek <abhiswc29@gmail.com>
2022-07-21 23:56:14 +0530

    Day18-2

---
+++ Day18/zigZag.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def zigzagLevelOrder(self, root):
+        if not root:
+            return []
+        res, temp, stack, flag = [], [], [root], 1
+        while stack:
+            for i in range(len(stack)):
+                node = stack.pop(0)
+                temp += [node.val]
+                if node.left:
+                    stack += [node.left]
+                if node.right:
+                    stack += [node.right]
+            res += [temp[::flag]]
+            temp = []
+            flag *= -1
+        return res
